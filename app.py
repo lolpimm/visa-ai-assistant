f83941d0e74a5e6bd06f48e5a9e7080133d698bd
Aung Kaung Myat <kaungmyat12345653@gmail.com>
2026-03-11 17:59:55 +0700

    Add chat interface UI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import json
 import psycopg2
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
@@ -15,7 +15,7 @@ db_url = os.getenv("DATABASE_URL")
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<h1>Visa AI Assistant is Live!</h1><p>Send a POST request to /generate-reply to chat.</p>"
+    return render_template('index.html')
 client = genai.Client(api_key=api_key)
 
 # ---------------------------------------------------------
